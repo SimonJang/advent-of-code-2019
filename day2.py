681038b67ee8a541e6da96c541d4b934e6dd0985
Simon Jang <simon.jang@into.care>
2019-12-03 15:29:43 +0100

    Day 2 - solution 2

--- day2.py
+++ day2.py
@@ -8,6 +8,7 @@ def execute_operation(operation: int, a: int, b: int) -> int:
     if operation is 99:
         return None
 
+    print(f"Exception operation: {operation}")
     raise Exception
 
 
@@ -19,10 +20,9 @@ def day1():
             instructions[1] = 12
             instructions[2] = 2
 
-            abort = False
             index = 0
 
-            while not abort:
+            while True:
                 operation = instructions[index]
                 value_a = instructions[instructions[index + 1]]
                 value_b = instructions[instructions[index + 2]]
@@ -37,4 +37,42 @@ def day1():
                 index = index + 4
 
 
-day1()
+# day1()
+
+
+def day2():
+    with open('./data/day2.txt') as file:
+        for line in file:
+            instructions = line.split(',')
+            instructions = [int(instruction) for instruction in instructions]
+            parameters = [tuple([x, y]) for x in range(0, 100) for y in range(0, 100)]
+
+            checksum = 19690720
+
+            for a, b in parameters:
+                calculated_instructions = instructions.copy()
+                index = 0
+
+                calculated_instructions[1] = a
+                calculated_instructions[2] = b
+
+                while True:
+                    operation = calculated_instructions[index]
+                    value_a = calculated_instructions[calculated_instructions[index + 1]]
+                    value_b = calculated_instructions[calculated_instructions[index + 2]]
+                    store_location = calculated_instructions[index + 3]
+
+                    result = execute_operation(operation, value_a, value_b)
+
+                    if result is None:
+                        if calculated_instructions[0] == checksum:
+                            print(f"{calculated_instructions[0]} | {checksum}")
+                            print(f"Checksum match: {100 * a + b}")
+
+                        break
+
+                    calculated_instructions[store_location] = result
+                    index = index + 4
+
+
+day2()
